247e5c977a66f7fa21144d4182a8014c60fef0f1
Alif Munim <alif.munim@ryerson.ca>
2020-11-16 22:19:26 -0500

    Add product and user routes base

--- backend/server.js
+++ backend/server.js
@@ -2,8 +2,10 @@ import express from "express";
 import colors from "colors";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js"
+
 import productRoutes from "./routes/productRoutes.js"
-import { notFound, errorHandler } from "./middleware/errorMiddleware.js" 
+import userRoutes from "./routes/userRoutes.js"
 
 const app = express();
 dotenv.config();
@@ -13,10 +15,11 @@ app.get("/", (req, res) => {
     res.send("API is running...");
 });
 
+app.use(express.json());
 app.use("/api/products", productRoutes);
+app.use("/api/users", userRoutes);
 
 app.use(notFound);
-
 app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
