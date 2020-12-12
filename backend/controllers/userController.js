68350d6027b995b516a5ebbbea3414694088831d
Alif Munim <alif.munim@ryerson.ca>
2020-12-12 14:38:35 -0500

    Add admin auth middleware

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -105,6 +105,14 @@ const updateUserProfile = asyncHandler(async (req, res) => {
     }
 });
 
+// @desc    Get user profiles
+// @route   GET /api/users
+// @access  Private/admin
+const getUsers = asyncHandler(async (req, res) => {
+    const users = await User.find({});
+    res.json(users);
+});
 
 
-export { authUser, getUserProfile, registerUser, updateUserProfile }
+
+export { authUser, getUserProfile, registerUser, updateUserProfile, getUsers }
