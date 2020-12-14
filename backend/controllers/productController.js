c13ba3d16195b17090e40ab44f85b31b46cc8d8d
Alif Munim <alif.munim@ryerson.ca>
2020-12-13 20:26:47 -0500

    Add custom page titles with react helmet

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -156,11 +156,21 @@ const createProductReview = asyncHandler(async (req, res) => {
     res.status(201).json(createdProduct);
 });
 
+// @desc    Get top products
+// @route   GET /api/products/top
+// @access  Public
+const getTopProducts = asyncHandler(async (req, res) => {
+
+    const topProducts = await Product.find({}).sort({ rating: - 1}).limit(3);
+    res.json(topProducts);
+});
+
 export {
     getProducts,
     getProductById,
     deleteProduct,
     createProduct,
     updateProduct,
-    createProductReview
+    createProductReview,
+    getTopProducts
 }
--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,4 +1,5 @@
 import React, { useEffect } from "react";
+import { Link } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { Row, Col } from "react-bootstrap";
 import Product from "../components/Product";
@@ -8,6 +9,8 @@ import Message from "../components/Message";
 import Loader from "../components/Loader";
 import Paginate from "../components/Paginate";
 import ProductCarousel from "../components/ProductCarousel"
+
+import Meta from "../components/Meta";
 
 
 const HomeScreen = ({ match }) => {
@@ -26,8 +29,14 @@ const HomeScreen = ({ match }) => {
 
 	return (
 		<>
-			{!keyword && <ProductCarousel />}
-			<h1>Latest Products</h1>
+			<Meta />
+			{!keyword ? (
+				<>
+					<ProductCarousel />
+					<h1>Latest Products</h1>
+				</>
+			) : <Link to="/" className="btn btn-light">Go Back</Link>}
+			
 			{loading ? (
 				<Loader />
 			) : error ? (
